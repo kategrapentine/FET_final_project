754e958a115269da8d98377f1c8881d3caaa01d1
kategrapentine <66195915+kategrapentine@users.noreply.github.com>
2020-08-23 01:02:46 -0700

    adding pizza loader

--- index.js
+++ index.js
@@ -243,3 +243,29 @@ function clearElement(element) {
         element.removeChild(element.firstChild);
     }
 }
+
+// pizza loader code 
+
+var loader;
+
+function loadNow(opacity){
+    if(opacity <=0){
+        displayContent();
+    }
+    else{
+        loader.style.opacity=opacity;
+        window.setTimeout(function(){
+            loadNow(opacity-0.05)
+        },100);
+    }
+}
+
+function displayContent(){
+    loader.style.display='none';
+    document.getElementById('content').style.display='block';
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    loader=document.getElementById('loader');
+    loadNow(1);
+});
